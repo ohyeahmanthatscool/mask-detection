10f88e05a66c783c56a5b6ecca641ca01ef32a99
ohyeahmanthatscool <98067439+ohyeahmanthatscool@users.noreply.github.com>
2022-02-15 08:00:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ mkdir model
 cd model
 mkdir mask-detect
 cd ../
-mv ssd-mobilenet.onnx.1.1.8001.GPU.FP16.engine ~/mask-detection/model/mask-detect
 mv ssd-mobilenet.onnx ~/mask-detection/model/mask-detect
 mv labels.txt ~/mask-detection/model/mask-detect
 chmod +x start.sh
